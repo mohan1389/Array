ed4a9a618f952f5d849109c412c1a183205115c8
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-09 18:35:27 +0530

    Create 53. Maximum Subarray Sum.cpp

---
+++ 53. Maximum Subarray Sum.cpp
@@ -0,0 +1,39 @@
+//BETTER
+//TC: O(n^2)
+//SC: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = -1e9;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum = sum + nums[j];
+                maxSum = max(sum,maxSum);
+            }
+        }
+
+        return maxSum;
+    }
+};
+
+
+//BRUTEFORCE
+//TC: O(n)
+//SC: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {//KADANE'S ALGORITHM
+        if(nums.size() == 1) return nums[0];
+        int maxSum = -1e9;
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+           sum = sum + nums[i];
+           maxSum = max(maxSum, sum);
+           if(sum < 0) sum = 0;
+        }
+        return maxSum;
+    }
+};
