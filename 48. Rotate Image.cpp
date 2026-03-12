8ad4c743555209879dc00742c24b80680a249e6f
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-13 00:21:27 +0530

    Add rotate function for 2D matrix
    Implement matrix rotation in place using two passes.

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -21,3 +21,27 @@ public:
         }
     }
 };
+
+
+//TC: O(n^2)
+//SC: O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            int left=0, right = n-1;
+            while(left <= right){
+                swap(matrix[i][left],matrix[i][right]);
+                left++;
+                right--;
+            }
+        }
+    }
+};
