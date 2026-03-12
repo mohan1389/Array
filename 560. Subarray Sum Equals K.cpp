23826bf648396c2d11abaedfe6229d0c9c29c020
mohan1389 <146318390+mohan1389@users.noreply.github.com>
2026-03-12 15:14:26 +0530

    Add optimal subarray sum solution using hashmap
    Implemented an optimal solution for finding the number of subarrays that sum to k using a hashmap.

--- 560. Subarray Sum Equals K.cpp
+++ 560. Subarray Sum Equals K.cpp
@@ -16,3 +16,27 @@ public:
         return cnt;
     }
 };
+
+
+//OPTIMAL
+// Time  = O(n)
+// Space = O(n)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0, cnt = 0, n =nums.size();
+        unordered_map<int,int> mpp;
+        mpp[0] = 1;
+
+        for(int i=0; i<n; i++){
+            sum = sum + nums[i];
+           int prefixSum = sum - k;
+
+           if(mpp.find(prefixSum) != mpp.end()){
+                cnt += mpp[prefixSum];
+            }
+           mpp[sum]++;
+        }
+        return cnt;
+    }
+};
